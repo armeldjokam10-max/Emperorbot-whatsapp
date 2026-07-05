9e1c7632f22ab1368e8b614281c9e8e5379a9deb
armeldjokam10-max <armeldjokam10@gmail.com>
2026-07-05 12:52:54 +0100

    Create config/config.js

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    prefix: ".",
+    botName: "EmperorBot",
+    version: "2.0.0",
+    creator: "armeldjokam10-max",
+    sudoList: ["237651539195"],
+    youtubeApiKey: "AIzaSyDWcxAanP1LhLNMpnV8Ky6NKwHQYq6Kw5I",
+    geniusApiKey: "TuViH1uZpAHyKV_5OCpWNsTTw0V4B_iO4PsRXNq0m4PvYitsd1vE5tYI8tw0HbL2"
+};
