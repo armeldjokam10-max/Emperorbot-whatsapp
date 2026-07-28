80772146984fcf608f41f24ef50013c521f28f15
armeldjokam10-max <armeldjokam10@gmail.com>
2026-07-28 09:25:36 +0100

    Delete config.js

--- config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    prefix: ".",
-    botName: "EmperorBot",
-    version: "2.0.0",
-    creator: "petit empereur",
-    sudoList: ["237651539195"],
-    youtubeApiKey: "AIzaSyDWcxAanP1LhLNMpnV8Ky6NKwHQYq6Kw5I",
-    geniusApiKey: "TuViH1uZpAHyKV_5OCpWNsTTw0V4B_iO4PsRXNq0m4PvYitsd1vE5tYI8tw0HbL2"
-};
