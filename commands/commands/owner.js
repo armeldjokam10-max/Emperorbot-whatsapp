0957fcbb01906ca0bdaa371f1b675eb7ca1c6b55
armeldjokam10-max <armeldjokam10@gmail.com>
2026-08-05 09:15:20 +0100

    Create commands/owner.js

---
+++ commands/commands/owner.js
@@ -0,0 +1,36 @@
+module.exports = {
+    name: "owner",
+    category: "Général",
+
+    async execute({ sock, from, config }) {
+
+        await sock.sendMessage(from, {
+            text: `
+╭━━━〔 👑 CRÉATEUR SYSTEM 👑 〕━━━╮
+
+⚔️ *YAMATO END0*
+
+━━━━━━━━━━━━━━━━━━
+
+👤 Développeur :
+${config.owner.name}
+
+🤖 Projet :
+${config.botName}
+
+🧠 Rôle :
+Architecte du système
+
+━━━━━━━━━━━━━━━━━━
+
+"Chaque empire commence
+par une vision."
+
+━━━━━━━━━━━━━━━━━━
+
+👑 Petitampure
+`
+        });
+
+    }
+};
